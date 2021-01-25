d5214e5680e863b5ab17e294a64273eb21acf785
snamiki1212 <snamiki1212@gmail.com>
2021-01-25 00:04:10 -0800

    Update pre-process as insert css impor line

--- scripts/lib.js
+++ scripts/lib.js
@@ -1,5 +1,5 @@
 export const process = (componentName) => {
-  const htmlDir = convertComponentNameToDir(componentName)
+  const htmlDir = convertComponentNameToDir(componentName);
   fetch(htmlDir)
     .then((stream) => {
       return stream.text();
@@ -11,7 +11,7 @@ export const process = (componentName) => {
     });
 };
 
-const convertComponentNameToDir = (name) => `./components/${name}.html`
+const convertComponentNameToDir = (name) => `./components/${name}.html`;
 
 const converStringHtmlToNode = (html) => {
   const elem = document.createElement("div");
@@ -19,16 +19,25 @@ const converStringHtmlToNode = (html) => {
   return elem.firstChild;
 };
 
+const cssImportLine = `<link href="css/reset.css" rel="stylesheet" />`;
+
 const defineCustomElement = (id) => {
   customElements.define(
     id,
     class extends HTMLElement {
       constructor() {
         super();
+
+        // get element
         const elem = document.getElementById(id);
         if (!elem) return console.error(`Cannot find element by id: ${id}`);
         const content = elem.content;
+
+        // pre process
+        content.appendChild(converStringHtmlToNode(cssImportLine));
         const clone = content.cloneNode(true);
+
+        // append
         this.attachShadow({ mode: "open" }).appendChild(clone);
       }
     }
