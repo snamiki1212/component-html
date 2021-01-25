9a7097345b5328c7705e2738be77c92ada267282
snamiki1212 <snamiki1212@gmail.com>
2021-01-24 21:35:13 -0800

    Refactor file

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,12 @@ import {process} from './lib.js';
 const list = [{
   html: "./components/super-man.html",
   id: "super-man",
+}, {
+  html: "./components/my-paragraph.html",
+  id: "my-paragraph",
+}, {
+  html: "./components/your-paragraph.html",
+  id: "your-paragraph",
 }];
 
 list.forEach(p => {
